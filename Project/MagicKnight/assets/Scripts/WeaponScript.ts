ebddad63cc7c1a7eeb2b022f7c8535918ac2f3e2
cty012 <tianyue.cao21@gmail.com>
2021-10-30 19:42:44 -0500

    Move weapon code to weapon script

--- Project/MagicKnight/assets/Scripts/WeaponScript.ts
+++ Project/MagicKnight/assets/Scripts/WeaponScript.ts
@@ -1,7 +1,7 @@
 
-import { _decorator, Component, Node, PhysicsSystem2D, Contact2DType, Collider2D, IPhysics2DContact, UITransform, RigidBody2D } from 'cc';
+import * as cc from 'cc';
 import { PlayerController } from './PlayerController';
-const { ccclass, property } = _decorator;
+const { ccclass, property } = cc._decorator;
 
 /**
  * Predefined variables
@@ -16,46 +16,103 @@ const { ccclass, property } = _decorator;
  */
  
 @ccclass('WeaponScript')
-export class WeaponScript extends Component {
+export class WeaponScript extends cc.Component {
     // [1]
     // dummy = '';
 
     // [2]
     // @property
     // serializableDummy = 0;
-    rigidBody: RigidBody2D;
-    collider: Collider2D;
-    uiTransform: UITransform;
-    player: Node;
+    rigidBody: cc.RigidBody2D;
+    collider: cc.Collider2D;
+    uiTransform: cc.UITransform;
+    player: cc.Node;
+    playerController: PlayerController;
     maxRotateTime: number;
     curRotateTime: number;
 
     isCollide: boolean;
 
+    //attack
+    private allowAttack: boolean;
+    private rotateStep: number;
+    private initialRotation: number;
+    private curAttackTime: number;
+    private maxAttackTime: number;
+
+    // weapom position
+    private weaponRightX: number;
+    private weaponY: number;
+
+    private keyAttack: cc.KeyCode;
+
     onLoad () {
-        this.rigidBody = this.getComponent(RigidBody2D);
-        this.collider = this.getComponent(Collider2D);
-        this.uiTransform = this.getComponent(UITransform);
+        this.rigidBody = this.getComponent(cc.RigidBody2D);
+        this.collider = this.getComponent(cc.Collider2D);
+        this.uiTransform = this.getComponent(cc.UITransform);
         this.player = this.node.getParent();
+        this.playerController = this.player.getComponent(PlayerController);
 
         this.maxRotateTime = 0.2;
         this.curRotateTime = 0;
         this.isCollide = false;
+
+        this.allowAttack = true;
+        this.rotateStep = -30;
+        this.initialRotation = -25;
+        this.curAttackTime = 0;
+        this.maxAttackTime = 0.15;
+
+        this.weaponRightX = 40;
+        this.weaponY = 30;
+
+        this.keyAttack = cc.KeyCode.KEY_J;
     }
 
     start () {
         if (this.collider) {
-            this.collider.on(Contact2DType.BEGIN_CONTACT, this.begineContact, this);
+            this.collider.on(cc.Contact2DType.BEGIN_CONTACT, this.begineContact, this);
+        }
+        // add a key down listener (when a key is pressed the function this.onKeyDown will be called)
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
+
+    onKeyDown (event: cc.EventKeyboard) {
+        switch (event.keyCode) {
+            case this.keyAttack:
+                if(!this.allowAttack) {
+                    break;
+                }
+                if (this.curAttackTime == 0)  {
+                    this.curAttackTime = this.maxAttackTime;
+                }
+                break;
         }
     }
 
-    begineContact (selfCollider: Collider2D, otherCollider: Collider2D, contact: IPhysics2DContact) {
+    begineContact (selfCollider: cc.Collider2D, otherCollider: cc.Collider2D, contact: cc.IPhysics2DContact) {
         console.log(selfCollider.node, otherCollider.node);
         this.isCollide = true;
     }
 
     update (deltaTime: number) {
+        // follow player
+        if (this.playerController.facingright) {
+            this.node.setPosition(this.weaponRightX, this.weaponY);
+        } else {
+            this.node.setPosition(this.playerController.uiTransform.contentSize.width - this.weaponRightX, this.weaponY);
+        }
 
+        // attack
+        if (this.curAttackTime > 0) {
+            if (this.playerController.facingright) this.rigidBody.angularVelocity = this.rotateStep;
+            if (!this.playerController.facingright) this.rigidBody.angularVelocity = -this.rotateStep;
+            this.curAttackTime = Math.max(this.curAttackTime - deltaTime, 0);
+        } else {
+            this.rigidBody.angularVelocity = 0;
+            if (this.playerController.facingright) this.node.setRotationFromEuler(0, 0, this.initialRotation);
+            if (!this.playerController.facingright) this.node.setRotationFromEuler(0, 0, -this.initialRotation);
+        }
     }
 }
 
